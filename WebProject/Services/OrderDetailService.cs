dc58a749d6fa587b6cb365f243717b1a11ed552a
Karl.chen <k010165pid@gmail.com>
2025-03-04 00:18:52 +0800

    開發訂單明細，未完成

--- WebProject/Services/OrderDetailService.cs
+++ WebProject/Services/OrderDetailService.cs
@@ -37,16 +37,44 @@ namespace WebProject.Services
             await _guestModelContext.SaveChangesAsync();
         }
 
-        //public async Task<VMOrderDetail> GetVMOrderDetailByAccAndOrderNo(string acc, string orderNo)
-        //{
-        //    var memberID = await _memberServices.GetMemberIDByAccount(acc);
-        //    var order = await _orderServices.GetOrderByOrderNo(orderNo);
-            
-        //}
+        public async Task<VMOrderDetail> GetVMOrderDetailByAccAndOrderNo(string acc, string orderNo)
+        {
+            var memberID = await _memberServices.GetMemberIDByAccount(acc);
+            var order = await _orderServices.GetOrderByOrderNo(orderNo);
+            var listDetail = await GetOrderDetailByOrderNo(orderNo);
+            return await GetVMOrderDetailByOrderObject(order, listDetail);
+        }
 
-        //public async Task<List<OrderDetail>> GetOrderDetailBy(string memberID, string orderNo)
-        //{
-        //    var list = await _guestModelContext.OrderDetail.
-        //}
+        public async Task<List<OrderDetail>> GetOrderDetailByOrderNo(string orderNo)
+        {
+            var list = await _guestModelContext.OrderDetail.Where(c=> c.OrderNo == orderNo).ToListAsync();
+            return list;
+        }
+
+        private async Task<VMOrderDetail> GetVMOrderDetailByOrderObject(Order order, List<OrderDetail> orderDetail)
+        {
+            VMOrderDetail vMOrderDetail = new VMOrderDetail();
+            vMOrderDetail.OrderDate = order.OrderDate;
+            vMOrderDetail.ShippingDate = order.ShippingDate;
+            vMOrderDetail.sendWay = order.ShippingWay.ShippingWayName;
+            vMOrderDetail.ShippingAddr = order.ShippingAddr;
+            vMOrderDetail.OrderStatusID = order.Ordertatus.OrdertatusName;
+            vMOrderDetail.item = new List<VMOrderCarItem>();
+            foreach (var it in orderDetail)
+            {
+                var p = await _productsService.GetProductByID(it.ProductID);
+                VMOrderCarItem item1 = new VMOrderCarItem();
+                float price = p.CostJP * p.PriceExchangeRage;
+                item1.price = Convert.ToInt32((decimal)price);
+                item1.product = it.ProductID;
+                item1.name = p.ProductName;
+                item1.img = p.Photo;
+                item1.offset = it.Off;
+                item1.count = it.Vol;
+                vMOrderDetail.item.Add(item1);
+            }
+
+            return vMOrderDetail;
+        }
     }
 }

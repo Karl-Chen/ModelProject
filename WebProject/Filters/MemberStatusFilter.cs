353e3d9bd060293ac68b0af1ed70764784e6ea8d
Karl.chen <k010165pid@gmail.com>
2025-03-10 00:46:47 +0800

    修正登入非店員帳號時點會員專區也會導到店員權限的頁面

--- WebProject/Filters/MemberStatusFilter.cs
+++ WebProject/Filters/MemberStatusFilter.cs
@@ -13,19 +13,20 @@ namespace WebProject.Filters
         public void OnActionExecuting(ActionExecutingContext context)
         {
             var controller = context.RouteData.Values["controller"].ToString();
-            var action = context.RouteData.Values["action"];
+            string action = context.RouteData.Values["action"].ToString();
             //if (context.HttpContext.Session.GetString("Manager") == null)
             //{
             //    context.Result = new RedirectToActionResult("Login", "Login", null);
             //}
-            var admin = context.HttpContext.Session.GetString("Admin");
+            var Manager = context.HttpContext.Session.GetString("Manager");
+            var Admin = context.HttpContext.Session.GetString("Admin");
             if (context.HttpContext.Session.GetString("Admin") == null && controller == "ADProducts")
             {
                 context.Result = new RedirectToActionResult("Index", "Products", null);
             }
-            else if (context.HttpContext.Session.GetString("Admin") == null && controller == "Members" && action == "")
+            else if ((Admin == null) && controller == "Members" && action == "Index")
             {
-                context.Result = new RedirectToActionResult("Index", "Members", null);
+                context.Result = new RedirectToActionResult("UserDetails", "Members", null);
             }
         }
     }
